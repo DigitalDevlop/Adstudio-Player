dad9b9d9a609c1d86a274e1204f057baa1f9a84f
DigitalDevlop <150772482+DigitalDevlop@users.noreply.github.com>
2024-02-14 16:15:28 +0530

    Update AppFooter.js

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="https://adstudio.cloud" target="_blank" rel="noopener noreferrer">
+          Adstudio
         </a>
-        <span className="ms-1">&copy; 2023 creativeLabs.</span>
+        <span className="ms-1">&copy; 2024 creativeLabs.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
+        <a href="https://adstudio.cloud/" target="_blank" rel="noopener noreferrer">
+          Adstudio
         </a>
       </div>
     </CFooter>
